bd028552ec452b8d23bc6a725e265467fb1f4577
Adel Baselizadeh <adel.baselizadeh@gmail.com>
2026-07-04 18:42:47 +0200

    Updating the profile info

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://adelbaseli.github.io',
+
   vite: {
       plugins: [tailwindcss()],
   },
